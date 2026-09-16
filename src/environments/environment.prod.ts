0f5a01b5c9e3d99d7552ed36e3ce28d04553def1
Muhammad Syahidan <hidanyomaen@gmail.com>
2026-09-16 19:08:37 +0700

    feat: production environment + fileReplacements for deploy

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,6 @@
+export const environment = {
+  production: true,
+  // Ganti dengan URL Web Service BE di Render, contoh:
+  // apiUrl: 'https://logitrack-be-xxxx.onrender.com/api'
+  apiUrl: 'https://logitrack-be.onrender.com/api'
+};
